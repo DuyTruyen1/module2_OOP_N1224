f85320f492ef0bc528149d6836b1e10148664c69
DuyTruyen1 <truyenmap420@gmail.com>
2025-01-08 13:59:36 +0700

    Regex

--- src/s11_regex/main.java
+++ src/s11_regex/main.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input;
+        //a
+        System.out.println("DTC123".matches("DTC[0-9]{3}"));
+        System.out.println("DTC123".matches("DTC\\d{3}"));
 
-        System.out.print("Vui lòng nhập điện thoại cũ với mã DTCXXXXX (với XXX là số): ");
-        input = sc.nextLine();
+        //b
+        System.out.println("+8409847323".matches("[+][(](0|84)[)]3[2-9][0-9]{7}"));
 
-//        System.out.println(input.matches("DTC[0-9]{3}"));
-        System.out.println(input.matches("^(0|\\+84)(3[2-9])\\d{7}$"));
+        //c
+        System.out.println("Pham_Duy_Truyen_2003@gmail.com".matches("[a-zA-Z0-9_]{6,32}[@]gmail[.]com"));
 
-//        System.out.println("0351234567".matches("([(]\\+84[)]"));
-
+        //d
+        System.out.println("Pham_DuyTruyen.".matches("[a-zA-Z0-9_.]{6,32}"));
     }
 }
